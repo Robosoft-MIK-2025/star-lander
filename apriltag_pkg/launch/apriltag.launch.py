7207bb4e010989454f44b4e98893648090f72b38
- <perkinsokunek@gmail.com>
2025-08-22 13:44:47 +0300

    fix: исправлена ошибка с конфигами apriltag_ros + удобнее конфиг для rviz

--- apriltag_pkg/launch/apriltag.launch.py
+++ apriltag_pkg/launch/apriltag.launch.py
@@ -2,25 +2,37 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
+
+    apriltag_config = os.path.join(
+        get_package_share_directory('apriltag_pkg'),
+        'config',
+        'tags.yaml'
+    )
+    # wrong
+    #     PathJoinSubstitution([
+    #     FindPackageShare('apriltag_pkg'),
+    #     'config',
+    #     'tags.yaml'
+    # ])
+
     return LaunchDescription([
         Node(
             package='apriltag_ros',
             executable='apriltag_node',
-            name='apriltag_node',
+            name='apriltag',
             output='screen',
             remappings=[
                 ('image_rect', '/image_raw'),  # Топик изображения из v4l2_camera
                 ('camera_info', '/camera_info')  # Калибровка камеры
             ],
             parameters=[
-                PathJoinSubstitution([
-                    FindPackageShare('apriltag_pkg'),
-                    'config',
-                    'tags.yaml'
-                ])
+                apriltag_config
             ],
-            arguments=['--ros-args', '--log-level', 'apriltag_node:=debug']
+            arguments=['--ros-args', '--log-level', 'apriltag:=ERROR'] # debug
         )
     ])
